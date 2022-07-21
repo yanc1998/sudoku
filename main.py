54dc9cc48c5f50029afa934793035f55e27cf5c2
yank1 <yancarloglez98@gmail.com>
2022-07-21 14:40:12 -0400

    candidates, finish

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    board = [[5, 9, 8, 7, 4, 3, 1, 6, 2],
+    board = [[0, 9, 8, 7, 4, 3, 1, 6, 2],
              [7, 6, 4, 2, 9, 1, 5, 3, 8],
              [1, 3, 2, 6, 5, 8, 4, 7, 9],
-             [8, 7, 1, 9, 6, 2, 3, 4, 5],
-             [3, 4, 9, 8, 1, 5, 7, 2, 6],
+             [8, 7, 0, 9, 0, 2, 3, 4, 5],
+             [3, 4, 9, 8, 0, 5, 7, 2, 6],
              [2, 5, 6, 4, 3, 7, 8, 9, 1],
              [4, 2, 7, 5, 8, 6, 9, 1, 3],
              [9, 1, 5, 3, 2, 4, 6, 8, 7],
@@ -26,7 +26,9 @@ if __name__ == '__main__':
 
     sudoku = Sudoku(board)
     a = sudoku.check_sudoku_sol(board)
+    b = sudoku.create_sudoku_candidates(board)
     print(a)
+    print(b)
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- src/sudoku.py
+++ src/sudoku.py
@@ -4,26 +4,69 @@ class Sudoku:
 
     def check_sudoku_sol(self, board):
         n = len(board[0])
-        for posx in range(0, n - 2, 3):
-            for posy in range(0, n - 2, 3):
-                if posx == posy == 6:
-                    print('a')
-                if not self.check_3x3_square(board, posx, posy):
-                    return False
+        for posx, posy in self.pos_board(n - 2, n - 2, 3):
+            if posx == posy == 6:
+                print('a')
+            if not self.check_3x3_square(board, posx, posy):
+                return False
 
         return True
 
     def check_3x3_square(self, board, posx, posy):
         visit_pos = {}
         dis = 3
-        for x in range(dis):
-            for y in range(dis):
-                nx = x + posx
-                ny = y + posy
-                value = board[nx][ny]
-                if visit_pos.__contains__(value) or (value < 0 or value > 9):
-                    return False
-                else:
-                    visit_pos[value] = True
+        for x, y in self.pos_board(dis, dis, 1):
+            nx = x + posx
+            ny = y + posy
+            value = board[nx][ny]
+            if visit_pos.__contains__(value) or (value < 0 or value > 9):
+                return False
+            else:
+                visit_pos[value] = True
 
         return True
+
+    def create_sudoku_candidates(self, board):
+        empty_pos = self.find_empty_pos(board)
+        candidates = {}
+        for x, y in empty_pos:
+            candidates[(x, y)] = self.valid(board, x, y)
+        return candidates
+
+    def valid(self, board, posx, posy):
+        col = self.find_column(board, posx, posy)
+        fil = self.find_fil(board, posx, posy)
+        valid = []
+        for i in range(1, 10):
+            if not col.__contains__(i) and not fil.__contains__(i):
+                valid.append(i)
+
+        return valid
+
+    def find_fil(self, board, _posx, _posy):
+        take_values = set()
+        for i in range(_posx, len(board[0])):
+            take_values.add(board[i][_posy])
+        for i in range(_posx, -1, -1):
+            take_values.add(board[i][_posy])
+        return take_values
+
+    def find_column(self, board, _posx, _posy):
+        take_values = set()
+        for i in range(_posy, len(board)):
+            take_values.add(board[_posx][i])
+        for i in range(_posx, -1, -1):
+            take_values.add(board[_posx][i])
+        return take_values
+
+    def find_empty_pos(self, board):
+        list_index_empty = []
+        for x, y in self.pos_board(len(board[0]), len(board), 1):
+            if board[x][y] == 0:
+                list_index_empty.append((x, y))
+        return list_index_empty
+
+    def pos_board(self, x, y, increment):
+        for i in range(0, x, increment):
+            for j in range(0, y, increment):
+                yield i, j
